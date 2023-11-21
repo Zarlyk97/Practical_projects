9c9b44d46a2c5a666f273fc85dd3816086130b61
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2023-11-21 22:40:24 +0700

    changed

---
+++ rick_and_morty/lib/feature/presentation/widgets/custom_search_delegate.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  CustomSearchDelegate() : super(searchFieldLabel: 'Search for characters...');
+  final _suggestions = [
+    'Rick',
+    'Morty',
+    'Summer',
+    'Beth',
+    'Jerry',
+  ];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+            showSuggestions(context);
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(
+        Icons.arrow_back_outlined,
+      ),
+      tooltip: 'Back',
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        return Text(
+          _suggestions[index],
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w400,
+          ),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
+      itemCount: _suggestions.length,
+    );
+  }
+}
