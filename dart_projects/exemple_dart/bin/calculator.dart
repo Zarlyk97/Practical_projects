5d952061f5299f9e0234760d7281f6d220c3daa0
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2023-12-06 11:25:09 +0700

    done

--- dart_projects/exemple_dart/bin/calculator.dart
+++ dart_projects/exemple_dart/bin/calculator.dart
@@ -1,71 +1,145 @@
+// class Calculator {
+//   late double _result;
+
+//   // Конструктор по умолчанию
+//   Calculator() {
+//     _result = 0.0;
+//   }
+
+//   // Геттер для получения результата
+//   double get result => _result;
+
+//   // Метод для сложения
+//   void add(double value) {
+//     _result += value;
+//   }
+
+//   // Метод для вычитания
+//   void subtract(double value) {
+//     _result -= value;
+//   }
+
+//   // Метод для умножения
+//   void multiply(double value) {
+//     _result *= value;
+//   }
+
+//   // Метод для деления
+//   void divide(double value) {
+//     if (value != 0) {
+//       _result /= value;
+//     } else {
+//       print("Ошибка: Деление на ноль!");
+//     }
+//   }
+
+//   // Метод для сброса результата
+//   void reset() {
+//     _result = 0.0;
+//   }
+// }
+
+// void main() {
+//   // Пример использования класса Calculator
+//   Calculator myCalculator = Calculator();
+
+//   myCalculator.add(5.0);
+//   print("Результат после сложения: ${myCalculator.result}");
+
+//   myCalculator.multiply(2.0);
+//   print("Результат после умножения: ${myCalculator.result}");
+
+//   myCalculator.subtract(3.0);
+//   print("Результат после вычитания: ${myCalculator.result}");
+
+//   myCalculator.divide(2.0);
+//   print("Результат после деления: ${myCalculator.result}");
+
+//   // Сброс результата
+//   myCalculator.reset();
+//   print("Результат после сброса: ${myCalculator.result}");
+// }
+
+// int add(int a, int b) {
+//   return a + b;
+// }
+
+// int subtract(int a, int b) {
+//   return a - b;
+// }
+
+// int multiply(int a, int b) {
+//   return a * b;
+// }
+
+// double division(double a, double b) {
+//   return a / b;
+// }
+
+// int modulus(int a, int b) {
+//   // modulus
+//   return a - b;
+// }
+
+// void main() {
+//   print(add(24, 9));
+//   print(subtract(7, 3));
+//   print(multiply(4, 50));
+//   print(division(120, 4));
+//   print(modulus(9, 3));
+// }
 import 'dart:io';
 
+class MathOperations {
+  double add(double a, double b) => a + b;
+  double subtract(double a, double b) => a - b;
+  double multiply(double a, double b) => a * b;
+  double divide(double a, double b) => a / b;
+}
+
 class Screen {
-  // Бул getInput методу, бул сандарды жана опеарация белгисин клавиатурадан окуп List түрүндө кайтарат.
-  getInput() {
-    double a = double.parse(stdin.readLineSync()!);
-    String op = stdin.readLineSync()!;
-    double b = double.parse(stdin.readLineSync()!);
-    return [a, op, b];
-  }
-}
+  MathOperations math = MathOperations();
 
-class Math {
-  add(a, b) => a + b;
+  void queryNumbersAndOperation() {
+    print("Enter the first number:");
+    double num1 = double.parse(stdin.readLineSync()!);
 
-  subtract(a, b) {
-    var joop = a - b;
-    print(joop);
+    print("Enter the second number:");
+    double num2 = double.parse(stdin.readLineSync()!);
+
+    print("Choose operation (+, -, *, /):");
+    String operation = stdin.readLineSync()!;
+
+    performOperation(num1, num2, operation);
   }
 
-  multiply(a, b) {
-    var result1 = a * b;
-    print(result1);
-  }
+  void performOperation(double num1, double num2, String operation) {
+    double result;
 
-  divide(a, b) {
-    var result = a / b;
-    print(result);
-  }
-}
-
-// Calculator бул негизги класс Screen жана Math объектерин түзөт
-class Calculator {
-  late Screen screen;
-  late Math math;
-
-  Calculator() {
-    screen = Screen();
-    math = Math();
-  }
-
-//operate() методу калкуляцияны жүргүзүү үчүн колдонулат
-// Ал screen ден сандарды жана операцияны окуп алат
-// Операцияга жараша Math классынын  тиешелүү методун чакырат  (мисалы:кошуу болсо add(), эгер бөлүү болсо Divide()ушул сыяктуу).
-// Эсептөөнүн жообун басат
-// бул 3 класс өз ара аракеттенип калькуляторду түзүшөт.
-  operate() {
-    List input = screen.getInput();
-    double a = input[0];
-    String op = input[1];
-    double b = input[2];
-
-    if (op == '+') {
-      print('баштадык');
-      print(math.add(a, b));
-      print('баштадык');
-    } else if (op == '-') {
-      print(math.subtract(a, b));
-    } else if (op == '*') {
-      print(math.multiply(a, b));
-    } else if (op == '/') {
-      print(math.divide(a, b));
+    switch (operation) {
+      case '+':
+        result = math.add(num1, num2);
+        break;
+      case '-':
+        result = math.subtract(num1, num2);
+        break;
+      case '*':
+        result = math.multiply(num1, num2);
+        break;
+      case '/':
+        result = math.divide(num1, num2);
+        break;
+      default:
+        print("Invalid operation");
+        return;
     }
+
+    print("Result: $result");
   }
 }
 
 void main() {
-  Calculator calc = Calculator();
-  calc.operate();
-  print(calc);
+  Screen calculatorScreen = Screen();
+  calculatorScreen.queryNumbersAndOperation();
+  print(calculatorScreen);
 }
