39aed8d9ecd46fe3383c259d97986f364f8908fc
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-01-04 01:41:14 +0700

    done

--- eco_market/lib/features/maine_screen/presentation/pages/search/search_screen.dart
+++ eco_market/lib/features/maine_screen/presentation/pages/search/search_screen.dart
@@ -11,6 +11,15 @@ class SeachScreen extends StatefulWidget {
 }
 
 class _SeachScreenState extends State<SeachScreen> {
+  List<String> fruitsNames = [
+    'Все',
+    'Фрукты',
+    'Сухофрукты',
+    'Овощи',
+    'Зелень',
+    'Чай кофе',
+    'Молочные продукты',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +27,68 @@ class _SeachScreenState extends State<SeachScreen> {
         iconTheme: const IconThemeData(color: AppColors.black),
         title: const Text(
           'Продукты',
-          style: TextStyle(color: AppColors.black),
+          style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+              color: AppColors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 12, left: 16, right: 16),
+        child: SafeArea(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  constraints: const BoxConstraints(
+                    maxHeight: 46,
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xffF8F8F8),
+                  hintText: 'Быстрый поиск',
+                  hintStyle: const TextStyle(color: Color(0xffD2D1D5)),
+                  prefixIcon:
+                      const Icon(Icons.search, color: Color(0xffD2D1D5)),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(16)),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                height: 38,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => GestureDetector(
+                          onTap: () {},
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: AppColors.white,
+                                border:
+                                    Border.all(color: const Color(0xffD2D1D5))),
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                              child: Text(
+                                fruitsNames[index],
+                                style: const TextStyle(
+                                  color: Color(0xffD2D1D5),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(width: 8),
+                    itemCount: fruitsNames.length),
+              ),
+            ],
+          ),
         ),
       ),
     );
