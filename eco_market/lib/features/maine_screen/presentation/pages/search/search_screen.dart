4d694f09e6979d00f9d88070f01ea27241d79315
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-01-05 21:26:43 +0700

    I finished searchPage and made the first page of show bottomsheet

--- eco_market/lib/features/maine_screen/presentation/pages/search/search_screen.dart
+++ eco_market/lib/features/maine_screen/presentation/pages/search/search_screen.dart
@@ -13,16 +13,8 @@ class SeachScreen extends StatefulWidget {
 
 class _SeachScreenState extends State<SeachScreen> {
   List<Item> items = List.generate(8, (index) => Item());
+  List<FruitsName> fruits = List.generate(7, (index) => FruitsName());
 
-  List<String> fruitsNames = [
-    'Все',
-    'Фрукты',
-    'Сухофрукты',
-    'Овощи',
-    'Зелень',
-    'Чай кофе',
-    'Молочные продукты',
-  ];
   int _currentIndex = 0;
 
   @override
@@ -67,36 +59,39 @@ class _SeachScreenState extends State<SeachScreen> {
                 height: 38,
                 child: ListView.separated(
                     scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) => GestureDetector(
-                          onTap: () => _onTabTapped(index),
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: _currentIndex == index
-                                    ? AppColors.green
-                                    : AppColors.white,
-                                border: Border.all(
-                                    color: _currentIndex == index
-                                        ? AppColors.white
-                                        : AppColors.grey)),
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                              child: Text(
-                                fruitsNames[index],
-                                style: TextStyle(
+                    itemBuilder: (context, index) {
+                      FruitsName fruit = fruits[index];
+                      return GestureDetector(
+                        onTap: () => _onTabTapped(index),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: _currentIndex == index
+                                  ? AppColors.green
+                                  : AppColors.white,
+                              border: Border.all(
                                   color: _currentIndex == index
                                       ? AppColors.white
-                                      : AppColors.grey,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
+                                      : AppColors.grey)),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                            child: Text(
+                              fruit.fruitsName[index],
+                              style: TextStyle(
+                                color: _currentIndex == index
+                                    ? AppColors.white
+                                    : AppColors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
                           ),
                         ),
+                      );
+                    },
                     separatorBuilder: (context, index) =>
                         const SizedBox(width: 8),
-                    itemCount: fruitsNames.length),
+                    itemCount: fruits.length),
               ),
               const SizedBox(
                 height: 20,
@@ -115,76 +110,87 @@ class _SeachScreenState extends State<SeachScreen> {
                     return GestureDetector(
                       onTap: () {},
                       child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: const Color(0xffF8F8F8),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(6.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Center(
-                                  child: Image.asset(
-                                    'assets/images/search/apples.png',
-                                    fit: BoxFit.cover,
-                                  ),
+                        decoration: BoxDecoration(
+                          color: const Color(0xffF8F8F8),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(
+                                child: Image.asset(
+                                  index % 2 == 0
+                                      ? 'assets/images/search/apples.png'
+                                      : 'assets/images/search/orange.png',
+                                  fit: BoxFit.cover,
                                 ),
-                                const SizedBox(
-                                  height: 4,
+                              ),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                index % 2 == 0
+                                    ? 'Яблоко красная радуга сладкая'
+                                    : 'Апельсины сладкий пакистанский',
+                                style: const TextStyle(
+                                  color: AppColors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
                                 ),
-                                const Text(
-                                  'Яблоко красная радуга сладкая',
-                                  style: TextStyle(
-                                    color: AppColors.black,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                  ),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                index % 2 == 0 ? '56 с' : '86 с',
+                                style: const TextStyle(
+                                  color: AppColors.green,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
                                 ),
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                                const Text(
-                                  '56 с',
-                                  style: TextStyle(
-                                    color: AppColors.green,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 14,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    IconButtonWidget(
-                                        icon: Icons.remove,
-                                        onTap: () {
-                                          setState(() {
-                                            item.decrementCounter();
-                                          });
-                                        }),
-                                    Text(
-                                      item.getCounter().toString(),
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w500,
-                                      ),
+                              ),
+                              const SizedBox(
+                                height: 14,
+                              ),
+                              index % 2 == 0
+                                  ? Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        IconButtonWidget(
+                                            icon: Icons.remove,
+                                            onTap: () {
+                                              setState(() {
+                                                item.decrementCounter();
+                                              });
+                                            }),
+                                        Text(
+                                          item.getCounter().toString(),
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        IconButtonWidget(
+                                            icon: Icons.add,
+                                            onTap: () {
+                                              setState(() {
+                                                item.incrementCounter();
+                                              });
+                                            })
+                                      ],
+                                    )
+                                  : CustomButtomWidget(
+                                      height: 32,
+                                      text: 'Добавить',
+                                      onPressed: () => showRule(context),
                                     ),
-                                    IconButtonWidget(
-                                        icon: Icons.add,
-                                        onTap: () {
-                                          setState(() {
-                                            item.incrementCounter();
-                                          });
-                                        })
-                                  ],
-                                )
-                              ],
-                            ),
-                          )),
+                            ],
+                          ),
+                        ),
+                      ),
                     );
                   },
                 ),
@@ -200,5 +206,85 @@ class _SeachScreenState extends State<SeachScreen> {
     setState(() {
       _currentIndex = index;
     });
+  }
+
+  showRule(BuildContext context) => showModalBottomSheet<void>(
+        isScrollControlled: true,
+        context: context,
+        // showDragHandle: true,
+        backgroundColor: Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        builder: (BuildContext context) => Padding(
+          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+          child: Wrap(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/images/search/orange2.png',
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Text(
+                    'Апельсины сладкий пакистанский',
+                    style: TextStyle(
+                      color: AppColors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    '56 c шт',
+                    style: TextStyle(
+                      color: Color(0xFF75DB1B),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  text(
+                    'Cочный плод яблони, который употребляется в пищу в свежем и запеченном виде, служит сырьём в кулинарии и для приготовления напитков.',
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  CustomButtomWidget(
+                    text: 'Добавить',
+                    onPressed: () {},
+                    height: 54,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+
+  Widget text(
+    String text,
+  ) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Color(0xFFABABAD),
+        fontSize: 16,
+      ),
+    );
   }
 }
