250349d01a43933513fdc4c59a3faafc03418a4e
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-01-12 21:07:31 +0700

    I have linked the search page API

--- eco_market/lib/service_locator.dart
+++ eco_market/lib/service_locator.dart
@@ -3,6 +3,9 @@ import 'package:eco_market/features/main/data/datasources/main_remote_data_sourc
 import 'package:eco_market/features/main/data/repositories/main_repo_impl.dart';
 import 'package:eco_market/features/main/domain/repositories/main_repository.dart';
 import 'package:eco_market/features/main/presentation/cubit/main_screen_cubit.dart';
+import 'package:eco_market/features/search/data/datasources/search_remote_data_source.dart';
+import 'package:eco_market/features/search/data/repositories/search_repo_imple.dart';
+import 'package:eco_market/features/search/domain/repositories/search_repository.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -13,6 +16,10 @@ Future<void> init() async {
   sl.registerLazySingleton<MainRepoistory>(() => MainRepoImpl(sl()));
   sl.registerLazySingleton<MainRemoteDataSource>(
       () => MainRemoteDataSourceImpl(sl()));
+
+  sl.registerLazySingleton<SearchRepository>(() => SearchRepoImple(sl()));
+  sl.registerLazySingleton<SearchRemoteDataSourse>(
+      () => SearchRemoteDataSourseImple(sl()));
 
   sl.registerLazySingleton(() => Dio());
 }
