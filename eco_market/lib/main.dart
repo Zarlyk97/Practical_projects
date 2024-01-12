8ab5ceba0ec9912193eb6ccfd186abc0f64a5c7f
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-01-12 21:07:16 +0700

    I have linked the search page API

--- eco_market/lib/main.dart
+++ eco_market/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:eco_market/config/config.dart';
 import 'package:eco_market/config/router/router.dart';
+import 'package:eco_market/features/main/presentation/cubit/main_screen_cubit.dart';
+import 'package:eco_market/features/search/presentation/cubit/search_screen_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'service_locator.dart' as di;
 
 void main() async {
@@ -21,10 +24,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
 
-    return MaterialApp.router(
-      theme: theme,
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter.config(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MainScreenCubit>(
+          create: (context) => di.sl<MainScreenCubit>()..getCategory(),
+        ),
+        BlocProvider<SearchScreenCubit>(
+            create: (context) => di.sl<SearchScreenCubit>()..getProducts()),
+      ],
+      child: MaterialApp.router(
+        theme: theme,
+        debugShowCheckedModeBanner: false,
+        routerConfig: appRouter.config(),
+      ),
     );
   }
 }
