b3c8b9939f207219f07cce59832b818f3f924a32
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-02-10 14:52:18 +0700

    done

--- eco_market/lib/features/main/presentation/widgets/item_class.dart
+++ eco_market/lib/features/main/presentation/widgets/item_class.dart
@@ -3,19 +3,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Items {
   int counter = 0;
 
-  void incrementCounter() async {
+  Future incrementCounter() async {
     final prefs = await SharedPreferences.getInstance();
     counter < 50 ? counter = (prefs.getInt('counter') ?? 0) + 1 : null;
     prefs.setInt('counter', counter);
   }
 
-  void decrementCounter() async {
+  Future decrementCounter() async {
     final prefs = await SharedPreferences.getInstance();
     counter > 0 ? counter = (prefs.getInt('counter') ?? 0) - 1 : null;
     prefs.setInt('counter', counter);
+    return null;
   }
 
-  void getCounter() async {
+  Future getCounter() async {
     final prefs = await SharedPreferences.getInstance();
     counter = (prefs.getInt('counter') ?? 0);
   }
