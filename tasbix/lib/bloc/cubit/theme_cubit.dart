bad7c01ecc40fcaf25479b2df9c75c41cd3c1272
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-03-24 15:37:21 +0700

    Implemented SharedPreferences to save data

--- tasbix/lib/bloc/cubit/theme_cubit.dart
+++ tasbix/lib/bloc/cubit/theme_cubit.dart
@@ -3,12 +3,19 @@ import 'dart:ui';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:tasbix/features/tasbix/domain/repositories/repositories.dart';
+
 part 'theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(const ThemeState(Brightness.light));
-
-  void toggleTheme(Brightness brightness) {
+  ThemeCubit({
+    required SettingRepositoryInterface settingRepository,
+  })  : _settingRepository = settingRepository,
+        super(const ThemeState(Brightness.light));
+  final SettingRepositoryInterface _settingRepository;
+  Future<void> toggleTheme(Brightness brightness) async {
     emit(ThemeState(brightness));
+    await _settingRepository
+        .setDarkThemeSelected(brightness == Brightness.dark);
   }
 }
--- tasbix/lib/main.dart
+++ tasbix/lib/main.dart
@@ -1,27 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:tasbix/bloc/cubit/theme_cubit.dart';
+import 'package:tasbix/features/tasbix/domain/repositories/repositories.dart';
 import 'package:tasbix/features/tasbix/presentation/cubit/tasbix_cubit.dart';
 import 'package:tasbix/ui/ui.dart';
 
 import 'features/tasbix/presentation/pages/pages.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  runApp(MyApp(
+    preferences: prefs,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({
+    super.key,
+    required this.preferences,
+  });
+  final SharedPreferences preferences;
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    final SettingRepository settingRepository = SettingRepository(
+      preferences: widget.preferences,
+    );
     return MultiBlocProvider(
       providers: [
         BlocProvider(
           create: (context) => TasbixCubit(),
         ),
         BlocProvider(
-          create: (context) => ThemeCubit(),
+          create: (context) => ThemeCubit(settingRepository: settingRepository),
         ),
       ],
       child: BlocBuilder<ThemeCubit, ThemeState>(
