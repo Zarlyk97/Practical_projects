38710c021206686ea45c91125bb7849ec15069dd
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-04-01 08:48:23 +0700

    finished

--- w_sharme_beauty_test/lib/features/main/presentation/pages/home/home_page.dart
+++ w_sharme_beauty_test/lib/features/main/presentation/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+import 'package:w_sharme_beauty_test/features/main/presentation/widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -35,63 +37,89 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: ListView.separated(
-          itemBuilder: (context, index) => SizedBox(
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(color: Colors.white),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  child: Text("A"),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: ListView.separated(
+            itemBuilder: (context, index) => SizedBox(
+                  child: DecoratedBox(
+                    decoration: const BoxDecoration(color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  const CircleAvatar(
+                                    child: Text("A"),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Elena Ivanovna",
+                                        style: theme.textTheme.bodyMedium!
+                                            .copyWith(
+                                                fontWeight: FontWeight.w700),
+                                      ),
+                                      Text(
+                                        "22-март в 10:00",
+                                        style: theme.textTheme.bodySmall!
+                                            .copyWith(
+                                                color: theme.disabledColor),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              CustomButton(theme: theme)
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Нет ничего более удивительного, чем мастерство маникюриста, который обладает умением превратить обычные ногти в истинные произведения искусства. Моя цель - не просто ухаживать за ногтями, но и придавать им индивидуальность, отражающую ваш стиль и характер.',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              ContainerWidget(
+                                text: '221',
+                                widget:
+                                    SvgPicture.asset('assets/svg/share.svg'),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              ContainerWidget(
+                                widget:
+                                    SvgPicture.asset('assets/svg/comment.svg'),
+                                text: '45',
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              ContainerWidget(
+                                text: '6',
+                                widget: SvgPicture.asset(
+                                  'assets/svg/comment.svg',
+                                  color: theme.primaryColor,
                                 ),
-                                const SizedBox(width: 10),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Elena Ivanovna",
-                                      style: theme.textTheme.bodyMedium!
-                                          .copyWith(
-                                              fontWeight: FontWeight.w700),
-                                    ),
-                                    Text(
-                                      "22-март в 10:00",
-                                      style: theme.textTheme.bodySmall!
-                                          .copyWith(color: theme.disabledColor),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                            ElevatedButton(
-                                style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  )),
-                                  backgroundColor: MaterialStateProperty.all(
-                                      theme.primaryColor),
-                                ),
-                                onPressed: () {},
-                                child: const Text("Подписаться",
-                                    style: TextStyle(color: Colors.white)))
-                          ],
-                        )
-                      ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-          separatorBuilder: (context, index) => const SizedBox(height: 10),
-          itemCount: 10),
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
+            itemCount: 10),
+      ),
     );
   }
 }
---
+++ w_sharme_beauty_test/lib/features/main/presentation/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'custom_button.dart';
+export 'container_widget.dart';
