05af871102ea3f2e0c0a4c056fbc0923188d8385
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-04-13 22:46:43 +0700

    name

--- dart_lessons/bin/lesson5.dart
+++ dart_lessons/bin/lesson5.dart
@@ -1,1 +1,12 @@
+void main() {
+  for (var element in ClassName().name) {
+    if (element == "Amir") {
+      break;
+    }
+    print(element);
+  }
+}
 
+class ClassName {
+  List<String> name = ["Zarlyk", "Amir", "Jumagul, Amir, Zarlyk"];
+}
