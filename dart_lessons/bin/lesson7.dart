82efc584214f8b585eb218800ccef3b467c54da8
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-04-20 19:37:52 +0700

    set

--- dart_lessons/bin/lesson7.dart
+++ dart_lessons/bin/lesson7.dart
@@ -1,3 +1,9 @@
 void main() {
   print("Hello, World!");
+  final set = {'one', 'two', 'three', 'four'};
+  print(set);
+  set.add('five');
+  print(set);
+  print(set.contains('four'));
+  print(set.contains('six'));
 }
