f4b48cfe6a2c9a46d676c1187fcf253f9b92aabd
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-04-21 18:28:06 +0700

    itarable

--- dart_lessons/bin/lesson7.dart
+++ dart_lessons/bin/lesson7.dart
@@ -1,9 +1,25 @@
 void main() {
-  print("Hello, World!");
-  final set = {'one', 'two', 'three', 'four'};
-  print(set);
-  set.add('five');
-  print(set);
-  print(set.contains('four'));
-  print(set.contains('six'));
+  // final setA = {1, 2, 3, 4, 5, 6};
+  // final setB = {3, 4, 5, 6, 7, 8};
+  // print(setA.union(setB));
+  // print(setA.intersection(setB));
+  // print(setA.difference(setB));
+  // print(setA..addAll(setB));
+  final myList = ['a', 'b', 'c'];
+  print(myList);
+  for (var element in myList) {
+    print(element);
+  }
+  final reverhod = myList.reversed;
+  print(reverhod);
+
+  // print("Hello, World!");
+  // final set = {'one', 'two', 'three', 'four'};
+  // print(set);
+  // set.add('five');
+  // print(set);
+  // print(set.contains('four'));
+  // set.remove('four');
+  // print(set.contains('six'));
+  // print([1, 2, 3]);
 }
