1c59aef9ce6d9f4638464a44b526b89019e913c4
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-04-30 20:03:01 +0700

    operation if finished

--- dart_lessons/bin/lesson8.dart
+++ dart_lessons/bin/lesson8.dart
@@ -1,10 +1,9 @@
 void main() {
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  print(numbers);
-  numbers.removeAt(2);
-  print(numbers);
-  numbers.removeLast();
-  print(numbers);
-  int result = numbers.reduce((value, element) => value + element);
-  print(result);
+  int a = 15;
+  int b = 15;
+  if (a < b && a != b) {
+    print("yes");
+  } else if (a >= b) {
+    print("no");
+  }
 }
