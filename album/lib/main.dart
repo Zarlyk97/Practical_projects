bcc7270fa363a753cf12ff0a1c380216bc2e3276
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-05-03 22:11:14 +0700

    /////////////new project

--- album/lib/main.dart
+++ album/lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:album/project/home.dart';
 import 'package:flutter/material.dart';
-
-import 'features/album/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const HomeScreen(),
     );
   }
 }
