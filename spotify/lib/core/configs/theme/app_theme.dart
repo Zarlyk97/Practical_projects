4a9accd22082ef1ecbbddde2a91fabb09e9f66db
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-07-17 15:09:28 +0700

    added font family Satoshi

--- spotify/lib/core/configs/theme/app_theme.dart
+++ spotify/lib/core/configs/theme/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     brightness: Brightness.light,
+    fontFamily: 'Satoshi',
     scaffoldBackgroundColor: AppColors.lightBackground,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
@@ -20,6 +21,7 @@ class AppTheme {
   static final darkTheme = ThemeData(
     primaryColor: AppColors.primary,
     brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
     scaffoldBackgroundColor: AppColors.darkBackground,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
