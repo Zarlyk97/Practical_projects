8c4df90a9bf85073b086d5108cd52ff1ccbc8e58
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-07-19 22:25:30 +0700

    ..........

--- spotify/lib/presentation/splash/pages/splash.dart
+++ spotify/lib/presentation/splash/pages/splash.dart
@@ -25,7 +25,7 @@ class _SplashPageState extends State<SplashPage> {
   }
 
   Future<void> redirect() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 2));
     Navigator.pushReplacement(context,
         MaterialPageRoute(builder: (context) => const GetStartedPage()));
   }
