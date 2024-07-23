2ad600ca29963817c6bf07c8b78898056b0d84d8
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-07-23 15:44:19 +0700

    done

--- spotify/lib/presentation/intro/pages/get_started.dart
+++ spotify/lib/presentation/intro/pages/get_started.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class GetStartedPage extends StatefulWidget {
   const GetStartedPage({super.key});
@@ -10,8 +13,46 @@ class GetStartedPage extends StatefulWidget {
 class _GetStartedPageState extends State<GetStartedPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Get Started Page')),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBG),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.logo)),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening To music ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.white),
+                ),
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.white),
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Get Started')),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          )
+        ],
+      ),
     );
   }
 }
