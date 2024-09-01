6e3e9667f4cb5c60cc90358446a2696e0f39f7a4
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-09-01 17:04:49 +0700

    done

---
+++ spotify/lib/common/widgets/appbar/app_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BasicAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  @override
+  Size get preferredSize => throw UnimplementedError();
+}
--- spotify/lib/presentation/auth/pages/sign_up_page.dart
+++ spotify/lib/presentation/auth/pages/sign_up_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -10,6 +12,22 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 40,
+          width: 40,
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
