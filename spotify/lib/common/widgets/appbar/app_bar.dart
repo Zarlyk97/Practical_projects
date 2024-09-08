fe638078ad03094496a15c70e665b35ede8c3438
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-09-09 00:03:42 +0700

    done

--- spotify/lib/common/widgets/appbar/app_bar.dart
+++ spotify/lib/common/widgets/appbar/app_bar.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
 
-class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const BasicAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-
-  @override
-  Size get preferredSize => throw UnimplementedError();
-}
--- spotify/lib/presentation/auth/pages/sign_up_page.dart
+++ spotify/lib/presentation/auth/pages/sign_up_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -14,10 +15,18 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back_ios),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Icon(Icons.arrow_back),
         ),
+        // SizedBox(
+        //     height: 30,
+        //     width: 30,
+        //     child: DecoratedBox(
+        //         decoration: BoxDecoration(
+        //       borderRadius: BorderRadius.circular(15),
+        //       color: Colors.black,
+        //     ))),
         title: SvgPicture.asset(
           AppVectors.logo,
           height: 40,
@@ -25,9 +34,29 @@ class _SignUpPageState extends State<SignUpPage> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _registerText(),
+            _fullNameField(),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _registerText() {
+    return const Text(
+      "Register",
+      style: TextStyle(
+          fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _fullNameField() {
+    return TextField();
   }
 }
