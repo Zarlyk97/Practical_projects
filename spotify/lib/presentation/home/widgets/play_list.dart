9f9ed3d41be404da6c9cd76988f0562d0dbd68c8
zarlyk.zhanybekov.bj@gmail.com <zarlyk.zhanybekov.bj@gmail.com>
2024-09-29 23:54:25 +0700

    ///////////

--- spotify/lib/presentation/home/widgets/play_list.dart
+++ spotify/lib/presentation/home/widgets/play_list.dart
@@ -112,7 +112,7 @@ class PlayList extends StatelessWidget {
                     icon: const Icon(Icons.favorite_rounded,
                         color: AppColors.darkGrey))
               ],
-            )
+            ),
           ],
         );
       },
